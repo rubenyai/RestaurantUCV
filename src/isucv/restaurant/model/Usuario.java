1edad0e3a97ceb44f20e5312e22b500d8f0324f1
Ruben Maza <rubenyai@outlook.com>
2015-07-15 23:28:50 -0430

    Creacion de Usuario.class, usuarios.class y lectura de archivo
    -Creacion de clase Usuario
    -Creacion de clase usuarios
    -Implementada lectura de archivo desde C:// (para poder probarlo deben
    mover users.txt a la raiz de C, esto sera cambiado pronto)
    
    **Creacion de Arraylist de objetos en cola de trabajo
    
    **Validacion de archivo en cola de trabajo

---
+++ src/isucv/restaurant/model/Usuario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isucv.restaurant.model;
+
+/**
+ *
+ * @author Ruben
+ */
+
+// Clase usuario, que sera objeto de usuarios para crear un array 
+public class Usuario {
+    private String user;
+    private String pass;
+    private int task;    
+
+//metodo para set de la clase usuario
+public Usuario(String usr,String psw, int tsk)
+{
+    user=usr;
+    pass=psw;
+    task=tsk;
+}
+
+//Metodos get para pruebas, estos no estan contemplados en el diseño, borrar en fase final
+public String getuser() {return user;}
+public String getpass() {return pass;}
+public int gettask() {return task;}
+/////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/isucv/restaurant/model/Usuarios.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isucv.restaurant.model;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Usuarios {
+    private ArrayList <Usuario> Users=null;
+    private boolean FileLoaded=false;
+    
+    public boolean CheckLogIn(String User, String Pass){
+       
+        return false; 
+    }
+    
+    public void LoadFile(){
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\users.txt")))
+		{
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null) {
+				//Prueba si esta funcionando bien la lectura
+                                System.out.println(sCurrentLine);
+                                ///////////////////////////////////////////
+                                //Creamos los objetos con new array list de usuario con los datos
+                                Users=new ArrayList <Usuario>();
+			}
+ 
+		} catch (IOException e) {
+                        throw new RuntimeException(e);
+		} 
+    }
+    
+    public int GetTaskForUser(String User){
+        return 0;
+    }
+}
