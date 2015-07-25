ffd20ff709d94f48f5b2a1de86ce051912643f4c
Flemidra <davidc1001@hotmail.com>
2015-07-24 19:48:30 -0430

    Correccion de errores
    Correccion de llaves en un IF de la interfaz despacho pedidos

--- src/isucv/restaurant/view/WndDespachoPedidos.java
+++ src/isucv/restaurant/view/WndDespachoPedidos.java
@@ -197,9 +197,13 @@ public class WndDespachoPedidos extends javax.swing.JFrame {
                     Descripcion = despacho.GetSpecialities().get(i).GetSides().get(j).GetSide().GetName();
                     Cantidad = despacho.GetSpecialities().get(i).GetSides().get(j).GetCount();
                     if (Cantidad == 1)
+                    {
                         Nuevo = new Object[] {null, Descripcion};
+                    }
                     else
-                    Nuevo = new Object[] {Cantidad, Descripcion};
+                    {
+                        Nuevo = new Object[] {Cantidad, Descripcion};
+                    }
                     md.addRow(Nuevo);
                 }
             } 
